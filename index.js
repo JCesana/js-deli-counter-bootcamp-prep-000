1a5b1bed57a9380ac6b7afffb1d59f85825785bd
Josh Cesana <joshuacesana@gmail.com>
2018-05-07 07:19:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 var katzDeli = [];
 
-function takeANumber(currentLine, name) {
-  currentLine.push(name);
+function takeANumber(customerLine, name) {
+  customerLine.push(name);
 }
 
 function nowServing() {
-  var nextCustomer = currentLine[0];
+  var nextCustomer = katzDeli[0];
   
-  if (currentLine.length === 0) {
+  if (katzDeli.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
-    currentLine.shift();
+    katzDeli.shift();
     return nextCustomer;
   }
 }
 
+function currentLine(customerLine) {
+  var line = "The line is currently: ";
+  
+  if (customerLine.length === 0) {
+    return "The line is currently empty.";
+  } else {
+    for (let i = 0; i < customerLine.length; i++) {
+      line += `${i + 1} ${customerLine[i]}, `;
+    }
+  }
+}
