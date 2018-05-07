638146e2398101c1dc7607d90bc12aa67d297ba1
Josh Cesana <joshuacesana@gmail.com>
2018-05-07 07:08:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ var katzDeli = [];
 function takeANumber(currentLine, name) {
   currentLine.push(name);
 }
+
+function nowServing() {
+  var nextCustomer = currentLine[0];
+  
+  if (currentLine.length === 0) {
+    return "There"
+  }
+  
+  currentLine.shift();
+  nowServing
+  return nextCustomer;
+}
